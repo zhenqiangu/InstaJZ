81bf9058c444260b51cc37d47ecf6a1472e27ee8
ZhenqianGu <zhenqianlgu@gmail.com>
2020-03-03 11:17:24 -0800

    Finish Website Development

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from Insta.models import Post, InstaUser, Like, UserConnection
+
+admin.site.register(Post)
+admin.site.register(InstaUser)
+admin.site.register(Like)
+admin.site.register(UserConnection)
+
--- Insta/urls.py
+++ Insta/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from Insta.views import HelloWorld
+from Insta.views import HelloWorld, PostsView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserDetailView, addLike
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name = 'helloworld'),
+    path('helloworld/', HelloWorld.as_view(), name = 'helloworld'),
+    path('', PostsView.as_view(), name = 'posts'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name = 'post_detail'),
+    path('post/new/', PostCreateView.as_view(), name = 'make_post'),
+    path('posts/update/<int:pk>/', PostUpdateView.as_view(), name = 'post_update'),
+    path('posts/delete/<int:pk>/', PostDeleteView.as_view(), name = 'post_delete'),
+    path('like', addLike, name='addLike'),
+    path('user/<int:pk>/', UserDetailView.as_view(), name = 'user_detail'),
 ]
